31b1b6adde9524ce62f04457b962cbbc2b059d06
Nasiphi Ngcobozi <nasiphigee@gmail.com>
2024-01-18 16:01:31 +0200

    revamped the whole porfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/**************typing animation******************/
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Content Creator", "Trainer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+})
